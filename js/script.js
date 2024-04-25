87314b36d3a36110d36a69a15ce7be3c2255763d
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-04-25 18:32:55 +0000

    cbn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+console.log(randomNumber)
+
+/**
+ * This function displays the slider value.
+ */
+
+const number = document.getElementById('number').checked
+
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById('on-check').checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("positive-number").innerHTML =
+    "<p>Value is: </p>" + randomNumber
+  } else {
+    randomNumber = (-1 * randomNumber)
+    document.getElementById("negative-number").innerHTML =
+    "<p>Value is: </p>" + randomNumber
+  }
+}
